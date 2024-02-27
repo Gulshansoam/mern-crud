b48bc196d012cacf58771bd2f42faf5d2b7f3fd8
gulshan <gulshansoam317@gmail.com>
2024-02-27 11:33:29 +0530

    fetch data and create workout detail

---
+++ frontend/src/components/WorkoutDetailComponent.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const WorkoutDetailComponent = (props) => {
+  const { workout, key } = props;
+  console.log(workout, "workout");
+  console.log(key, "key");
+  return (
+    <div className="workout-details">
+      <h4>{workout.title}</h4>
+      <p>
+        <strong>Load(in Kgs): </strong>
+        {workout.load}
+      </p>
+      <p>
+        <strong>Reps: </strong>
+        {workout.reps}
+      </p>
+      <p>{workout.createdAt}</p>
+    </div>
+  );
+};
+
+export default WorkoutDetailComponent;
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import WorkoutDetailComponent from "../components/WorkoutDetailComponent";
 
 const Home = () => {
+  const [allWorkouts, setAllWorkouts] = useState(null);
+
+  useEffect(() => {
+    const fetchWorkout = async () => {
+      const response = await fetch("/api/workouts");
+      const json = await response.json();
+      if (response.ok) setAllWorkouts(json);
+    };
+    fetchWorkout();
+  }, []);
+  console.log(allWorkouts, "allWorkouts");
   return (
     <div className="home">
-      <h2>Home</h2>
+      <div className="workouts">
+        {allWorkouts &&
+          allWorkouts?.map((workout) => (
+            <WorkoutDetailComponent workout={workout} key={workout._id} />
+          ))}
+      </div>
     </div>
   );
 };
